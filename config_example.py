649fc1d3e18355c1b1e3ed09073b8b23a9ca7530
nitzankatz2 <nitzankatz22@gmail.com>
2019-05-03 12:05:41 +0300

    add example

---
+++ config_example.py
@@ -0,0 +1,3 @@
+web_driver_path = r"C:\Users\nitzan\Downloads\chromedriver_win32\chromedriver.exe"
+data_dir = r'C:\dev\free_time_projects\kareoke\data'
+sox_path = r'C:\Program Files (x86)\sox-14-4-2'
