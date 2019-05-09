535f0de21a39a7d93b58c277adfd0164a0c37e4a
nitzankatz2 <nitzankatz22@gmail.com>
2019-05-09 09:34:17 +0300

    finisehd song convertion

--- convertion/convert_songs.py
+++ convertion/convert_songs.py
@@ -1,34 +1,35 @@
 import os
+import pandas as pd
 from constants.configs import config
 
 sox_path = config.sox_path
 main_dir = config.data_dir
+meta_data_file = config.meta_data_file
 
-def _mp3_to_wav(input_path, output_path, delete=False):
+def _mp3_to_wav(input_path, output_path):
     cmd = 'sox.exe ' + input_path + ' ' + output_path
     os.system(cmd)
-    if delete:
-        os.remove(input_path)
 
 
-def _make_path_absolute(path):
-    if not os.path.isabs(path):
-        path = os.path.join(os.curdir, path)
-    return path
-
-
-def convert_songs(path_list):
-    path_list = [_make_path_absolute(path) for path in path_list]
-    out_path_list = [path.replace('.mp3','.wav') for path in path_list]
-    initial_dir = os.curdir
-    os.chdir(sox_path)
-    for input_path, output_path in zip(path_list, out_path_list):
-        _mp3_to_wav(input_path, output_path)
-    os.chdir(initial_dir)
-    return out_path_list
+def convert_row(row):
+    if row['full_path'].endswith('.mp3'):
+        full_mp3 = row['full_path'].replace('.mp3','.wav')
+        _mp3_to_wav(row['full_path'],full_mp3)
+        row['relative_url'] = row['relative_url'].replace('.mp3','.wav')
+    return row
 
 def main():
-    dirs = main_dir
+    initial_dir = os.curdir
+    df = pd.read_csv(meta_data_file)
+    collumns = list(df.columns)
+    os.chdir(sox_path)
+    df['full_path'] = df['relative_url'].apply(lambda x: os.path.join(main_dir,x))
+    df = df.apply(convert_row,axis=1)
+    df = df.reset_index(drop=True)
+    df = df[collumns]
+    df.to_csv(config.meta_data_file, encoding='utf-8',index=False)
+    os.chdir(initial_dir)
+
 
 if __name__ == '__main__':
     # # os.chdir(sox_path)
