a8d9565b4971e7cfde1cb322f94dc7c06e352fd5
nitzankatz2 <nitzankatz22@gmail.com>
2019-05-11 16:52:36 +0300

    deleted config

--- constants/configs/config.py
+++
@@ -1,5 +0,0 @@
-web_driver_path = r"C:\Users\EYAL\Downloads\chromedriver_win32\chromedriver.exe"
-data_dir = r'C:\Users\EYAL\Desktop\eyal\nabaz\songs\ex1'
-sox_path = r'C:\Program Files (x86)\sox-14-4-2'
-meta_data_file = r'C:\dev\free_time_projects\kareoke\metadata\metadata.csv'
-sleep_time = 3
